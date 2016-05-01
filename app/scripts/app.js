52e3f14c8ec6182be3fa30b3f6480d9fc71ffb60
cborre <cborre2007@gmail.com>
2016-05-01 01:32:08 -0430

    tareas
    inicio de proyecto

---
+++ app/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tareasApp
+ * @description
+ * # tareasApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tareasApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+	'ui.sortable',
+	'LocalStorageModule'
+  ])
+  
+  .config(['localStorageServiceProvider', function (localStorageServiceProvider) {
+	  localStorageServiceProvider.setPrefix('ls');
+  }])
+  
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
